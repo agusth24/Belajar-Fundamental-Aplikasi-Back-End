de5928403a2670c0f58c1ef92888afcdad4913f8
agusth24 <agusthayono24@gmail.com>
2023-01-15 18:22:13 +0700

    fix bug host value production

--- notes-api-back-end-2/src/server.js
+++ notes-api-back-end-2/src/server.js
@@ -7,7 +7,7 @@ const init = async () => {
 	const notesServices = new NotesService();
 	const server = Hapi.server({
 		port: 5000,
-		host: 'localhost',
+		host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
 		routes: {
 			cors: {
 				origin: ['*'],
